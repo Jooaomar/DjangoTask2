c49d95db7c795e0523ea300f9d4dc2023dd9234d
João Marcelo Cunha <77308533+Jooaomar@users.noreply.github.com>
2023-05-11 12:53:25 -0300

    Update urls.py

--- task_api/urls.py
+++ task_api/urls.py
@@ -3,5 +3,5 @@ from task_api import views
 
 urlpatterns = [
     path('task_api/', views.task_list),
-    path('task_api/<int:pk>/', views.task_detail),
+    path('task_api/<str:pk>/', views.task_detail),
 ]
