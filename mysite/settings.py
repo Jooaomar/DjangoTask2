dea32e3c53568e67fde092c3aef79acc6ee476e4
João Marcelo <jmrcl91@gmail.com>
2023-05-11 12:27:22 -0300

    Cors

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,4 +144,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['https://serene-cranachan-7092d3.netlify.app/']
